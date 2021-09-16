2c3aeccbb587fd3aeece9c9f55617e0fc11bbab9
Tomee <1014723833@qq.com>
2021-09-16 20:36:54 +0800

    添加文件夹

---
+++ TransmissionBlockMod-Besiege/WheelBlock/HighSpeedWheelBloclScript.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Modding;
+using UnityEngine;
+
+
+class HighSpeedWheelBloclScript : BlockScript
+{
+    private MKey forwardKey, backwardKey;
+    private MSlider speedSlider, springSlider, damperSlider, acceleratedSlider, staticFrictionSlider, dynamicFrictionSlider, bouncinessSlider, massSlider;
+    private MToggle ignoreBaseColliderToggle, toggleToggle;
+
+    private ConfigurableJoint CJ;
+    private Vector3 lastScale;
+
+    public HighWheel Boxes;
+
+    public override void SafeAwake()
+    {
+        forwardKey = AddKey("Forward", "forward", KeyCode.UpArrow);
+        backwardKey = AddKey("Backward", "backward", KeyCode.DownArrow);
+        speedSlider = AddSlider("Speed", "speed", 1f, 0.1f, 3f);
+        springSlider = AddSlider("Spring", "Spring", 1f, 0.1f, 50f);
+        damperSlider = AddSlider("Damper", "Damper", 1f, 0.1f, 50f);
+        acceleratedSlider = AddSlider("Accelerated", "accelerated", 10f, 0f, 20f);
+        acceleratedSlider.maxInfinity = true;
+        staticFrictionSlider = AddSlider("Static Friction", "static friction", 0.5f, 0f, 1f);
+        dynamicFrictionSlider = AddSlider("Dynamic Friction", "dynamic friction", 0.8f, 0f, 1f);
+        bouncinessSlider = AddSlider("Bounciness", "bounciness", 0f, 0f, 1f);
+        massSlider = AddSlider("Mass", "mass", 0.25f, 0.05f, 2f);
+
+        toggleToggle = AddToggle("Toggle", "toggle", false);
+        ignoreBaseColliderToggle = AddToggle("Ignore Base" + Environment.NewLine + "Collider", "IBC", false);
+
+        lastScale = transform.localScale;
+
+        Rigidbody.inertiaTensorRotation = new Quaternion(0, 0, 0.4f, 0.9f);
+        Rigidbody.inertiaTensor = new Vector3(0.4f, 0.4f, 0.7f);
+        Rigidbody.drag = Rigidbody.angularDrag = 0f;
+
+        Rigidbody.solverIterations = 100;
+        Rigidbody.mass = 1f;
+
+        CJ = GetComponent<ConfigurableJoint>();
+        CJ.breakForce = CJ.breakTorque = Mathf.Infinity;
+    }
+
+    private event Action<Vector3, Vector3> onScale;
+    public override void BuildingUpdate()
+    {
+        if (transform.localScale != lastScale)
+        {
+            onScale?.Invoke(transform.localScale, lastScale);
+            setScale(transform.localScale, lastScale);
+
+            lastScale = transform.localScale;
+        }
+
+        void setScale(Vector3 currentScale, Vector3 lastScale)
+        {
+            var single = 0f;
+            if (currentScale.x != lastScale.x)
+            {
+                single = currentScale.x;
+            }
+            else if (currentScale.y != lastScale.y)
+            {
+                single = currentScale.y;
+            }
+            else
+            {
+                return;
+            }
+            transform.localScale = new Vector3(single, single, currentScale.z);
+            Boxes.SetRadius(Boxes.Radius * single);
+        }
+    }
+
+    public override void OnSimulateStart()
+    {
+        Rigidbody.maxAngularVelocity = 50f * speedSlider.Value;
+
+        Destroy(transform.FindChild("Boxes")?.gameObject);
+        Boxes = new HighWheel(transform, Rigidbody);
+        Boxes.RefreshBoxesCollider(springSlider.Value * 500f, damperSlider.Value * 250f, 5000f * springSlider.Value);
+        Boxes.SetBoxesPhysicMaterail(bouncinessSlider.Value, staticFrictionSlider.Value, dynamicFrictionSlider.Value);
+        Boxes.SetBoxesBodyAttribute(massSlider.Value);
+        if (ignoreBaseColliderToggle.IsActive)
+        {
+            StartCoroutine(ignore());
+        }
+
+        addDynamicAxis();
+
+        void addDynamicAxis()
+        {
+            CJ.axis = Vector3.forward;
+            CJ.secondaryAxis = Vector3.up;
+            CJ.angularXMotion = ConfigurableJointMotion.Free;
+
+            var jd = CJ.angularXDrive;
+            jd.maximumForce = 1000f;
+            //jd.positionDamper = 5000f;
+            CJ.angularXDrive = jd;
+
+        }
+
+        IEnumerator ignore()
+        {
+            yield return new WaitUntil(() => CJ.connectedBody != null);
+            Boxes.IgnorBaseBlockCollider();
+        }
+    }
+
+    float input = 0f, single = 0f, single1 = 0f;
+    public override void SimulateUpdateAlways()
+    {
+        if (!toggleToggle.IsActive)
+        {
+            input = 0f;
+            if (forwardKey.IsHeld)
+            {
+                input += 1f;
+                Rigidbody.WakeUp();
+            }
+            if (backwardKey.IsHeld)
+            {
+                input += -1f;
+                Rigidbody.WakeUp();
+            }
+        }
+        else
+        {
+            if (forwardKey.IsPressed)
+            {
+                input = input != 1f ? 1f : 0f;
+                Rigidbody.WakeUp();
+            }
+            if (backwardKey.IsPressed)
+            {
+                input = input != -1f ? -1f : 0f;
+                Rigidbody.WakeUp();
+            }
+        }
+
+        if (input == 0f)
+        {
+            single1 = Mathf.MoveTowards(single1, 750f, 10f * Time.deltaTime);
+            var jd = CJ.angularXDrive;
+            jd.positionDamper = single1;
+            CJ.angularXDrive = jd;
+        }
+        else
+        {
+            var jd = CJ.angularXDrive;
+            jd.positionDamper = 0f;
+            CJ.angularXDrive = jd;
+            single = Mathf.MoveTowards(single, 11.5f, input == 0f ? 0f : acceleratedSlider.Value * Time.deltaTime * 10f);
+            //CJ.targetAngularVelocity = Vector3.right * (Flipped ? -1f : 1f) * (CJ.swapBodies ? -1f : 1f) * input * speedSlider.Value * single;
+            Rigidbody.AddRelativeTorque(Vector3.forward * (Flipped ? -1f : 1f) /** (CJ.swapBodies ? -1f : 1f)*/ * input * speedSlider.Value * single, ForceMode.VelocityChange);
+        }
+
+
+        //Boxes.refreshVertices();
+    }
+    public override void SimulateFixedUpdateAlways()
+    {
+        //foreach (var box in Boxes.boxes)
+        //{
+        //    if (!box.rigidbody.useGravity)
+        //    {
+        //        box.rigidbody.AddForce(Vector3.down * 10f, ForceMode.Force);
+        //    }
+        //}
+    }
+}
+class HighWheel
+{
+    public GameObject gameObject;
+    public Box[] boxes;
+    public float Radius { get; set; } = 1.45f;
+
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+    private Transform parent;
+    private Rigidbody connectedBody;
+    public HighWheel(Transform parent, Rigidbody connectedBody)
+    {
+        this.parent = parent;
+        this.connectedBody = connectedBody;
+
+        gameObject = new GameObject("Boxes");
+        gameObject.transform.SetParent(parent);
+        gameObject.transform.position = parent.position;
+        gameObject.transform.rotation = parent.rotation;
+        gameObject.transform.localScale = parent.localScale;
+
+        var offect_forward = 0.5f;
+        var origin = gameObject.transform.localPosition;
+        var anchor = Vector3.forward * offect_forward;
+        //圆半径和旋转角
+        float radius = Radius / gameObject.transform.localScale.x;
+        float angle = 18f;
+        int index = (int)(360f / angle);
+
+        boxes = new Box[index];
+        //外圈box位置
+        for (var i = 0; i < index; i++)
+        {
+            var position = new Vector3(
+                                                origin.y + radius * Mathf.Sin(angle * i * Mathf.Deg2Rad),
+                                                origin.x - radius * Mathf.Cos(angle * i * Mathf.Deg2Rad),
+                                                offect_forward / gameObject.transform.localScale.z
+                                             );
+
+            boxes[i] = new Box(gameObject.transform, position, anchor, connectedBody, Radius);
+        }
+
+        for (var i = 0; i < index; i++)
+        {
+            for (var j = 0; j < index; j++)
+            {
+                Physics.IgnoreCollision(boxes[i].meshCollider, boxes[j].meshCollider);
+            }
+        }
+
+        meshFilter = gameObject.AddComponent<MeshFilter>();
+        meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        meshRenderer.material.color = Color.green;
+
+
+    }
+    public void SetRadius(float radius)
+    {
+        Radius = radius;
+
+        for (int i = 0; i < boxes.Length; i++)
+        {
+            boxes[i].SetRadius(radius);
+        }
+    }
+    public void SetBoxesColliderState(bool enabled)
+    {
+        foreach (var box in boxes)
+        {
+            var rb = box.rigidbody;
+            rb.detectCollisions = enabled;
+            rb.isKinematic = !enabled;
+        }
+    }
+    public void RefreshBoxesCollider(float spring, float damper, float maximumForce)
+    {
+        foreach (var box in boxes)
+        {
+            box.SetJointDrive(spring, damper, maximumForce);
+        }
+    }
+    public void SetBoxesPhysicMaterail(float bounciness, float staticFriction, float dynamicFriction)
+    {
+        foreach (var box in boxes)
+        {
+            box.SetPhysicMaterail(bounciness, staticFriction, dynamicFriction);
+        }
+    }
+
+    public void SetBoxesBodyAttribute(float mass)
+    {
+        foreach (var box in boxes)
+        {
+            box.SetBodyAttribute(true, mass);
+        }
+    }
+    public Vector3[] GetAllVertices()
+    {
+        var index = boxes.Length;
+        var vectors = new Vector3[index * 2];
+
+        var j = 0;
+        for (var i = 0; i < index; i++)
+        {
+            vectors[j++] = gameObject.transform./*parent.*/TransformPoint(boxes[i].GetVertices()[0]);
+            vectors[j++] = gameObject.transform./*parent.*/TransformPoint(boxes[i].GetVertices()[1]);
+        }
+        return vectors;
+    }
+    public void refreshVertices()
+    {
+        meshFilter.mesh.vertices = GetAllVertices();
+        var index = GetAllVertices().Length;
+        var uvs = new Vector2[index];
+        var tris = new int[(index - 2) * 3];
+        var j = 0;
+        for (var i = 0; i < index; i++)
+        {
+            uvs[i] = new Vector2(1.0f * i / index, 1);
+            if (j < tris.Length)
+            {
+                if (i % 2 != 0)
+                {
+                    tris[j++] = i;
+                    tris[j++] = i + 1;
+                    tris[j++] = i + 2 > index ? 0 : i + 2;
+                }
+                else
+                {
+                    tris[j++] = i + 2 > index ? i - 8 : i + 2;
+                    tris[j++] = i + 1 > index ? 0 : i + 1;
+                    tris[j++] = i;
+                }
+            }
+        }
+
+        meshFilter.mesh.uv = uvs;
+        meshFilter.mesh.triangles = tris;
+        meshFilter.mesh.RecalculateBounds();
+        meshFilter.mesh.RecalculateNormals();
+    }
+
+    public void IgnorBaseBlockCollider()
+    {
+        foreach (var col in connectedBody.gameObject.GetComponent<ConfigurableJoint>().connectedBody?.gameObject.GetComponentsInChildren<Collider>())
+        {
+            foreach (var box in boxes)
+            {
+                Physics.IgnoreCollision(box.meshCollider, col);
+            }
+        }
+    }
+}
+
+
